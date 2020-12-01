008cf4452f9e6d0861a400598c138a239ceb7142
antedeguemon <antedeguemon@users.noreply.github.com>
2020-12-01 20:46:20 -0300

    Update README

--- priv/diff.sh
+++ priv/diff.sh
@@ -1,7 +1,2 @@
 #/bin/bash
-git checkout master
-antedeguemon_checks --all > diff_1.txt
-git checkout $1
-antedeguemon_checks --all > diff_2.txt
-diff diff_1.txt diff_2.txt
-rm -rf diff_1.txt diff_2.txt
+git diff --name-only master | sed -e 's/^/--files-included=/' | xargs antedeguemon_checks
