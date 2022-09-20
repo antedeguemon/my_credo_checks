5b7e99a97b812ac4e421f4d42444c7165719a199
antedeguemon <antedeguemon@users.noreply.github.com>
2022-09-20 15:27:45 -0400

    Fixes ValidateDescribeArity for piped functions

--- lib/my_credo_checks/consistency/describe_arity.ex
+++ lib/my_credo_checks/consistency/describe_arity.ex
@@ -1,95 +1,107 @@
 defmodule MyCredoChecks.Consistency.DescribeArity do
-  use Credo.Check, param_defaults: [ignored_functions: []]
+  use Credo.Check, category: :consistency
+
+  # TODO: Make it configurable through parameters
+  @test_operations [:test, :test_with_params]
+  @describe_regex ~r/^([\w|\!|\?]+)\/([0-9]+)/i
 
   def run(source_file, params \\ []) do
     ast = SourceFile.ast(source_file)
+    issue_meta = IssueMeta.for(source_file, params)
 
-    context = [
-      issue_meta: IssueMeta.for(source_file, params),
-      module_name: Credo.Code.Module.name(ast),
-      ignored_functions: Keyword.get(params, :ignored_functions, [])
-    ]
-
-    Credo.Code.prewalk(ast, &traverse(&1, &2, context))
+    ast
+    |> Credo.Code.prewalk(&traverse_describe/2)
+    |> Enum.map(fn {meta, description, test} ->
+      format_issue(
+        issue_meta,
+        message:
+          "Test `#{test}` inside describe `#{description}` does not call its description function.",
+        trigger: "#{test} inside #{description}",
+        line_no: meta[:line]
+      )
+    end)
   end
 
-  defp traverse({:describe, _meta, [text, _block]} = ast, issues, context) do
-    if function_description?(text) do
-      describe_issues =
+  defp traverse_describe({:describe, _meta, [description, _block]} = ast, acc) do
+    new_issues =
+      if parsed_description = parse_description(description) do
         ast
-        |> Macro.unpipe()
-        |> Credo.Code.prewalk(&traverse_test(&1, &2, text, context))
+        |> Credo.Code.prewalk(&traverse_test(&1, &2, parsed_description))
+        |> Enum.map(fn {meta, test} -> {meta, description, test} end)
+      else
+        []
+      end
 
-      {ast, describe_issues ++ issues}
+    {ast, new_issues ++ acc}
+  end
+
+  defp traverse_describe(ast, acc) do
+    {ast, acc}
+  end
+
+  defp traverse_test({operation, meta, block} = ast, issues, {name, arity})
+       when operation in @test_operations do
+    calls =
+      block
+      |> Credo.Code.prewalk(&traverse_calls/2)
+      # Strips module name (for now). TODO: Consider module names
+      |> Enum.map(fn {_, name, arity} -> {name, to_string(arity)} end)
+
+    if {name, arity} not in calls do
+      {ast, [{meta, test_text(block)} | issues]}
     else
       {ast, issues}
     end
   end
 
-  defp traverse(ast, issues, _context) do
+  defp traverse_test(ast, issues, _text) do
     {ast, issues}
   end
 
-  defp traverse_test({:test, meta, block} = ast, issues, text, context) do
-    {function_name, arity} = parse_test_description(text)
+  defp traverse_calls({:|>, _meta, block} = ast, acc) do
+    {{target, _meta, arguments}, 0} = ast |> Macro.unpipe() |> List.last()
+    {module, name} = piped_call_signature(target)
+    call_signature = {module, name, length(arguments) + 1}
+    ast_without_current_call = List.delete_at(block, -1)
 
-    if {function_name, arity} in fetch_calls(block) or
-         function_name in context[:ignored_functions] do
-      {ast, issues}
-    else
-      {ast, [issue_for(test_text(block), text, meta, context) | issues]}
+    {ast_without_current_call, [call_signature | acc]}
+  end
+
+  defp traverse_calls({{:., _, [{:__aliases__, _, _}, _]}, _, _} = ast, acc) do
+    {ast, [call_signature(ast) | acc]}
+  end
+
+  defp traverse_calls(ast, acc) do
+    {ast, acc}
+  end
+
+  defp parse_description(description) do
+    case Regex.run(@describe_regex, description) do
+      [_, name, arity] -> {name, arity}
+      _ -> nil
     end
   end
 
-  defp traverse_test(ast, issues, _text, _context) do
-    {ast, issues}
+  defp piped_call_signature({:., _, [{:__aliases__, _, mod_list}, fun]}) do
+    module = Credo.Code.Name.last(mod_list)
+    name = Credo.Code.Name.full(fun)
+
+    {module, name}
   end
 
-  defp function_description?(text) do
-    String.match?(text, ~r/^(.*)\/[0-9]+/i)
+  defp piped_call_signature(fun) when is_atom(fun) do
+    name = Credo.Code.Name.full(fun)
+
+    {nil, name}
   end
 
-  defp issue_for(trigger_test, trigger_describe, meta, context) do
-    issue_meta = context[:issue_meta]
+  defp call_signature({{:., _, [{:__aliases__, _, mod_list}, fun]}, _, arguments}) do
+    module = Credo.Code.Name.last(mod_list)
+    name = Credo.Code.Name.full(fun)
 
-    format_issue(
-      issue_meta,
-      message:
-        "Test `#{trigger_test}` inside describe #{trigger_describe} does not call its description function.",
-      trigger: "#{trigger_test} inside #{trigger_describe}",
-      line_no: meta[:line]
-    )
+    {module, name, length(arguments)}
   end
 
   defp test_text([text | _]) when is_binary(text), do: text
   defp test_text(_), do: ""
-
-  defp parse_test_description(text) do
-    [function_text, arity_text | _] = String.split(text, "/")
-    {arity, _} = Integer.parse(arity_text)
-
-    {String.to_atom(function_text), arity}
-  end
-
-  defp fetch_calls(block) do
-    {_, symbol_table} = Macro.prewalk(block, [], &filter_calls/2)
-
-    Enum.map(symbol_table, &remove_alias/1)
-  end
-
-  defp filter_calls({symbol, _meta, block}, acc) when not is_nil(block) do
-    {block, acc ++ [{symbol, Enum.count(block)}]}
-  end
-
-  defp filter_calls(ast, acc) do
-    {ast, acc}
-  end
-
-  defp remove_alias({{:., _line, [{:__aliases__, _, _module}, function]}, arity}) do
-    {function, arity}
-  end
-
-  defp remove_alias({symbol, arity}) do
-    {symbol, arity}
-  end
 end
--- test/my_credo_checks/consistency/describe_arity_test.exs
+++ test/my_credo_checks/consistency/describe_arity_test.exs
@@ -1,14 +1,13 @@
 defmodule MyCredoChecks.Consistency.DescribeArityTest do
   use Credo.Test.Case
 
-  describe "ok" do
-    test "function is called without pipe" do
+  describe "function description" do
+    test "ignores trailing comments" do
       """
       defmodule ModuleTest do
-        describe "function/1" do
-          test ".." do
-            attrs = %{}
-            Module.function(attrs)
+        describe "run/1 | some comment about run/2" do
+          test "works" do
+            Module.run(attrs)
           end
         end
       end
@@ -18,13 +17,12 @@ defmodule MyCredoChecks.Consistency.DescribeArityTest do
       |> refute_issues()
     end
 
-    @tag :skip
-    test "function is called with pipe" do
+    test "leading comment implies on not being a function description" do
       """
       defmodule ModuleTest do
-        describe "function/1" do
-          test ".." do
-            attrs |> Module.function()
+        describe "this run/1" do
+          test "works" do
+            Module.not_run("hey", 1_234)
           end
         end
       end
@@ -38,13 +36,83 @@ defmodule MyCredoChecks.Consistency.DescribeArityTest do
       """
       defmodule ModuleTest do
         describe "works 123" do
-          test ".." do
+          test "not_a_describe/1" do
+            attrs |> Module.function()
+          end
+
+          test "not a definition 123" do
             attrs |> Module.function()
           end
         end
 
         test "works/1" do
           attrs |> Module.function()
+        end
+      end
+      """
+      |> to_source_file()
+      |> run_check(MyCredoChecks.Consistency.DescribeArity)
+      |> refute_issues()
+    end
+  end
+
+  describe "ok" do
+    test "function is called without pipe" do
+      """
+      defmodule ModuleTest do
+        describe "function/2" do
+          test ".." do
+            attrs = %{}
+            Module.function(attrs_1, attrs_2)
+          end
+        end
+      end
+      """
+      |> to_source_file()
+      |> run_check(MyCredoChecks.Consistency.DescribeArity)
+      |> refute_issues()
+    end
+
+    test "function is called with a multiple pipe" do
+      """
+      defmodule ModuleTest do
+        describe "fun/3" do
+          test "works" do
+            attrs
+            |> OtherModule.fun_1()
+            |> Module.fun(:argument, :other)
+            |> OtherModule.fun_2()
+          end
+        end
+      end
+      """
+      |> to_source_file()
+      |> run_check(MyCredoChecks.Consistency.DescribeArity)
+      |> refute_issues()
+    end
+
+    test "function is called with an inline pipe" do
+      """
+      defmodule ModuleTest do
+        describe "fun/3" do
+          test "works" do
+            attrs |> OtherModule.fun_1() |> Module.fun(:argument, :other) |> OtherModule.fun_2()
+          end
+        end
+      end
+      """
+      |> to_source_file()
+      |> run_check(MyCredoChecks.Consistency.DescribeArity)
+      |> refute_issues()
+    end
+
+    test "function is called with pipe for local function" do
+      """
+      defmodule ModuleTest do
+        describe "fun/3" do
+          test "works" do
+            attrs |> fun(1, 2)
+          end
         end
       end
       """
@@ -94,6 +162,42 @@ defmodule MyCredoChecks.Consistency.DescribeArityTest do
           end
         end
       end
+      """
+      |> to_source_file()
+      |> run_check(MyCredoChecks.Consistency.DescribeArity)
+      |> assert_issue()
+    end
+
+    test "arity minus one through a pipe" do
+      """
+      defmodule ModuleTest do
+        describe "function/2" do
+          test ".." do
+            1 |> run_1() |> Module.function() |> run_2()
+          end
+        end
+      end
+      |> to_source_file()
+      |> run_check(MyCredoChecks.Consistency.DescribeArity)
+      |> assert_issue()
+      """
+      |> to_source_file()
+      |> run_check(MyCredoChecks.Consistency.DescribeArity)
+      |> assert_issue()
+    end
+
+    test "arity plus one through a pipe" do
+      """
+      defmodule ModuleTest do
+        describe "function/1" do
+          test "works" do
+            1 |> run_1() |> Module.function(:second_argument) |> run_2()
+          end
+        end
+      end
+      |> to_source_file()
+      |> run_check(MyCredoChecks.Consistency.DescribeArity)
+      |> assert_issue()
       """
       |> to_source_file()
       |> run_check(MyCredoChecks.Consistency.DescribeArity)
